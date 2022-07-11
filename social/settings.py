79ff39c0eefdfb852c33ade5c87c55e923d701ae
Valtersen <71800322+Valtersen@users.noreply.github.com>
2022-07-11 19:27:07 +0300

    postgres

--- social/settings.py
+++ social/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogram',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
