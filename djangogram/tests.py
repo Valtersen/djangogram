75b1a35b808ef16ec410e1bd39bb8d706927d648
Valtersen <71800322+Valtersen@users.noreply.github.com>
2022-07-17 00:34:35 +0300

    tests

--- djangogram/tests.py
+++ djangogram/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.test import Client
+from allauth.utils import get_user_model
+from allauth.account.models import EmailAddress
+from django.urls import reverse
 
-# Create your tests here.
+from social import settings
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username = 'john'
+        self.email = 'smith@mail.com'
+        self.password = "password1"
+        self.user = get_user_model().objects.create(username=self.username, email=self.email)
+        self.user.set_password(self.password)
+        EmailAddress.objects.create(
+            user=self.user,
+            email=self.email,
+            primary=True,
+            verified=True
+        )
+
+    def test_login(self):
+        response = self.client.post('/login/',
+                                    {"Username": self.username, "Password": self.password})
+        self.assertRedirects(response, settings.LOGIN_REDIRECT_URL, fetch_redirect_response=False)
+
+
+
+
